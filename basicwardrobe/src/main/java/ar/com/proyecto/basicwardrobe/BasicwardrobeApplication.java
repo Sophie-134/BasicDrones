0ac0090f388db707e2bd6a5f92dd8ff08f19200a
Sophie-134 <sofiammendizabal@gmail.com>
2020-09-16 20:18:13 -0300

    Primer comit. Proyecto en blanco

---
+++ basicwardrobe/src/main/java/ar/com/proyecto/basicwardrobe/BasicwardrobeApplication.java
@@ -0,0 +1,13 @@
+package ar.com.proyecto.basicwardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicwardrobeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicwardrobeApplication.class, args);
+	}
+
+}
---
+++ basicwardrobe/src/test/java/ar/com/proyecto/basicwardrobe/BasicwardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.proyecto.basicwardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicwardrobeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
